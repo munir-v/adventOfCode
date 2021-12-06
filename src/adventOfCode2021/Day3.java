81dfb9bdb51b7649b7851d3363a295e7a0ce6364
munir-v <mvafai@protonmail.com>
2021-12-06 11:27:53 -0800

    day3 part 1

--- src/adventOfCode2021/Day3.java
+++ src/adventOfCode2021/Day3.java
@@ -6,12 +6,34 @@ import java.util.ArrayList;
 
 public class Day3 extends FileReader {
 
-	static File file = new File("AOC2021/day.txt");	
+	static File file = new File("AOC2021/day3.txt");	
 	static ArrayList<String> vals = new ArrayList<>();
 	
-	public static void part1() {
+	public static int part1() {
+		int gamma = 0;
+		int epsilon = 0;
+		String gammaStr = "";
+		String epsilonStr = "";
 		
-		
+		for (int i=0; i<vals.get(0).length(); i++) {
+			int zeroCount=0;
+			
+			for (int j=0; j<vals.size(); j++) {
+				int num = Integer.parseInt(vals.get(j).substring(i,i+1));
+				if(num==0) zeroCount++;
+			}
+			if(zeroCount<vals.size()/2) {
+				gammaStr+="0";
+				epsilonStr+="1";
+			}
+			else {
+				gammaStr+="1";
+				epsilonStr+="0";
+			}
+		}
+		gamma = Integer.parseInt(gammaStr,2);
+		epsilon = Integer.parseInt(epsilonStr,2);
+		return gamma*epsilon;
 	}
 	
 	public static void part2() {
@@ -21,7 +43,7 @@ public class Day3 extends FileReader {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		readFile(file,vals);
-		part1();
+		System.out.println(part1());
 		part2();
 		
 	}
