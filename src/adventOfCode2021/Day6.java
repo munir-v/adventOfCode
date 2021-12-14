c01148b954b6df3738fc43d57ab825763fe0f6e5
munir-v <mvafai@protonmail.com>
2021-12-13 19:45:36 -0800

    day 6 part 1

--- src/adventOfCode2021/Day6.java
+++ src/adventOfCode2021/Day6.java
@@ -3,15 +3,40 @@ package adventOfCode2021;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Day6 extends FileReader {
 
-	static File file = new File("AOC2021/day.txt");	
-	static ArrayList<String> vals = new ArrayList<>();
+	static File file = new File("AOC2021/day6.txt");	
+	static ArrayList<Integer> vals = new ArrayList<>();
+	static String input;
 	
-	public static void part1() {
-		
-		
+	//read file to array
+	private static void readString() {
+		String[] values = input.split(",");
+		for (String s : values) {
+			vals.add(Integer.parseInt(s));
+		}
+
+	}
+	
+	//find total # of fish
+	public static int part1() {
+		readString();
+
+		for (int i=0; i<80; i++) {
+			int max = vals.size();
+			for (int j=0; j<max; j++) {
+				int cur = vals.get(j);
+				if(cur==0) {
+					vals.set(j,6);
+					vals.add(8);
+				}
+				else vals.set(j,cur-1);
+			}
+		}
+
+		return vals.size();
 	}
 	
 	public static void part2() {
@@ -20,8 +45,8 @@ public class Day6 extends FileReader {
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		readFile(file,vals);
-		part1();
+		input = readFileToString(file);
+		System.out.println(part1());
 		part2();
 		
 	}
